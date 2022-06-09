0cd0bbaebbb3be77d9b227ed0f8d9fd32d62485a
Giovanna Betti da Silva Mendes <giovannabetti@gmail.com>
2022-06-09 09:13:30 -0300

    including welcome image

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
-require('dotenv').config()
+require('dotenv').config();
+
+const generateImage = require('./generateImage');
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 });
+
+const welcomeChannelId = '984201469647532144';
 
 client.on('ready', () => {
     console.log(`o bot tá on! ${client.user.tag}`);
@@ -16,6 +21,14 @@ client.on('messageCreate', (message) => {
     if (message.content == 'hi') {
         message.reply('hello will world!')
     }
-})
+});
+
+client.on('guildMemberAdd', async (member) => {
+    const img = await generateImage(member);
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+        });
+});
 
 client.login(process.env.TOKEN);
